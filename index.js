fb328f56822c6ec12299c2f68ca12998d6af6b28
Maximilian Stauss <max.stauss@gmail.com>
2015-03-31 12:52:53 +0200

    clean up

--- index.js
+++ index.js
@@ -2,25 +2,6 @@ var lescape       = require('escape-latex');
 var encArray      = require('./lib/t1_textcomp.js');
 var binaryIndexOf = require('./vendor/binaryIndexOf');
 
-// %% \CharacterTable
-// %%  {Upper-case    \A\B\C\D\E\F\G\H\I\J\K\L\M\N\O\P\Q\R\S\T\U\V\W\X\Y\Z
-// %%   Lower-case    \a\b\c\d\e\f\g\h\i\j\k\l\m\n\o\p\q\r\s\t\u\v\w\x\y\z
-// %%   Digits        \0\1\2\3\4\5\6\7\8\9
-// %%   Exclamation   \!     Double quote  \"     Hash (number) \#
-// %%   Dollar        \$     Percent       \%     Ampersand     \&
-// %%   Acute accent  \'     Left paren    \(     Right paren   \)
-// %%   Asterisk      \*     Plus          \+     Comma         \,
-// %%   Minus         \-     Point         \.     Solidus       \/
-// %%   Colon         \:     Semicolon     \;     Less than     \<
-// %%   Equals        \=     Greater than  \>     Question mark \?
-// %%   Commercial at \@     Left bracket  \[     Backslash     \\
-// %%   Right bracket \]     Circumflex    \^     Underscore    \_
-// %%   Grave accent  \`     Left brace    \{     Vertical bar  \|
-// %%   Right brace   \}     Tilde         \~}
-
-// REGEX to filter all charactars that are parsed without a problem
-NON_STANDARD_REGEX = /[^a-zA-Z\d\s!"#\$\%&'\(\)\*\+,\-\.\/:;<=>\?@\[\\\]\^_`\{\|\}~]/g;
-// All other characters have to be matches according to the fontenc
 
 var sanitizeLatex = function(str){
   var text = '';
